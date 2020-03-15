207671285ff0ddb06787a0204523f40e75839a99
jennyhuang1977 <jennyhuang1977@gmail.com>
2020-03-15 18:20:56 -0400

    my first maven commit

---
+++ src/main/java/org/jenny/javaplay/App.java
@@ -0,0 +1,15 @@
+package org.jenny.javaplay;
+import org.slf4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	//Logger logger = LoggerFactory.getLogger(App.class);
+ 	//logger.info("Hello, World!");
+    }
+}
